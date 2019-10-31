7686e680c08758b19cace04f8230f76e03cc6446
UpEight <milnerslm@gmail.com>
2019-10-31 16:52:48 +0000

    Remove unnecessary error handling from 'fetchTopics'

--- fe-nc-news/src/Components/ArticlesContainer/TopicsList.js
+++ fe-nc-news/src/Components/ArticlesContainer/TopicsList.js
@@ -3,18 +3,15 @@ import TopicCard from './TopicCard';
 import { Link } from '@reach/router';
 import LoadingIndicator from '../LoadingIndicator';
 import * as api from '../../api';
-import ErrorPage from '../ErrorPage';
 
 class TopicsList extends React.Component {
   state = {
     topics: [],
-    isLoading: true,
-    err: null
+    isLoading: true
   };
   render() {
-    const { topics, isLoading, err } = this.state;
+    const { topics, isLoading } = this.state;
     if (isLoading) return <LoadingIndicator />;
-    if (err) return <ErrorPage err={err} />;
     return (
       <section className="topics-list">
         <h2>Topics</h2>
@@ -40,12 +37,7 @@ class TopicsList extends React.Component {
   fetchTopics = () => {
     console.log('Fetching topics...');
     api.getTopics().then(topics => {
-      this.setState({ topics: topics, isLoading: false }).catch(err =>
-        this.setState({
-          err: { status: err.response.status, msg: err.response.data.msg },
-          isLoading: false
-        })
-      );
+      this.setState({ topics: topics, isLoading: false });
     });
   };
 }
