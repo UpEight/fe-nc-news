bf8166cd9ea855764f91379627d9ece18db21f24
UpEight <milnerslm@gmail.com>
2019-11-01 09:00:53 +0000

    Format date posted for articles and comments

--- fe-nc-news/src/Components/SingleArticleContainer/ArticleText.js
+++ fe-nc-news/src/Components/SingleArticleContainer/ArticleText.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const ArticleText = ({ article }) => {
+  const formattedDate = new Date(article.created_at);
   return (
     <div className="article-text">
       <p className="article-topic-header">{article.topic}</p>
       <p className="article-author-date-text">
-        Posted by {article.author} at {article.created_at}
+        Posted by {article.author} | {formattedDate.toString()}
       </p>
       <p className="article-title-text">{article.title}</p>
       <p className="article-body">{article.body}</p>
--- fe-nc-news/src/Components/SingleArticleContainer/CommentText.js
+++ fe-nc-news/src/Components/SingleArticleContainer/CommentText.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const CommentText = ({ comment }) => {
+  const formattedDate = new Date(comment.created_at);
+
   return (
     <div className="article-text">
       <p className="article-author-date-text">
-        Posted by {comment.author} at {comment.created_at}
+        Posted by {comment.author} | {formattedDate.toString()}
       </p>
       <p className="article-body">{comment.body}</p>
     </div>
