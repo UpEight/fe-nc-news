2ded324af79367709e54f2b0f780540b448bc0b8
UpEight <milnerslm@gmail.com>
2019-11-01 14:40:04 +0000

    Add site name

--- fe-nc-news/src/Components/Logo.js
+++ fe-nc-news/src/Components/Logo.js
@@ -3,8 +3,13 @@ import { Link } from '@reach/router';
 
 const Logo = () => {
   return (
-    <Link className="link logo" to="/">
-      <h1>NC News</h1>
+    <Link className="link" to="/">
+      <section className="logo no-margin">
+        <p className="logo-title">
+          Tidder{' '}
+          <span className="logo-text">...the back page of the internet</span>
+        </p>
+      </section>
     </Link>
   );
 };
