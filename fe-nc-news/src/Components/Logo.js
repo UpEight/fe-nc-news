d774f4cb8e7cc0ddea17633330f825c301bdf484
UpEight <milnerslm@gmail.com>
2019-11-03 11:53:34 +0000

    Add logo image to header

--- fe-nc-news/src/Components/Logo.js
+++ fe-nc-news/src/Components/Logo.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
+import logo from '../media/Gadget_Brain.jpg';
+
 const Logo = () => {
   return (
     <Link className="link" to="/">
       <section className="logo no-margin">
-        <p className="logo-title">
-          Tidder{' '}
-          <span className="logo-text">...the back page of the internet</span>
+        <img
+          src={logo}
+          className="logo-image"
+          id="logo"
+          alt="logo-brain-from-inspector-gadget"
+        ></img>
+        <p className="logo-title" id="logo-title">
+          Tidder
+        </p>
+        <p className="logo-text" id="logo-text">
+          ...the back page of the internet
         </p>
       </section>
     </Link>
