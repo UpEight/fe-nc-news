ca2cf29dce4f89304f4e2b38d2596c6a5b58584e
UpEight <milnerslm@gmail.com>
2019-11-03 16:00:56 +0000

    Add wrapper to create spacing between articles

--- fe-nc-news/src/Components/ArticlesContainer/ArticlesList.js
+++ fe-nc-news/src/Components/ArticlesContainer/ArticlesList.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import Article from '../SingleArticleContainer/Article';
+import ArticleMarginWrapper from '../ArticleMarginWrapper';
 
 const ArticlesList = ({ articles, containerType }) => {
   return (
     <ul className="articles-list no-padding">
-      {articles.map(article => (
-        <Article
-          key={article.article_id}
-          article={article}
-          containerType={containerType}
-        />
-      ))}
+      {articles.map(article =>
+        containerType === 'articles' ? (
+          <ArticleMarginWrapper>
+            <Article
+              key={article.article_id}
+              article={article}
+              containerType={containerType}
+            />
+          </ArticleMarginWrapper>
+        ) : (
+          <Article
+            key={article.article_id}
+            article={article}
+            containerType={containerType}
+          />
+        )
+      )}
     </ul>
   );
 };
